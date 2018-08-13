4dfc8fc2aec19d58ee58fff08a7a046e532bbac2
shebkoch <shebkoch@gmail.com>
2018-08-13 20:22:00 +0500

    fix

---
+++ Assets/Scripts/Gg.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using JetBrains.Annotations;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gg : MonoBehaviour {
+
+	public Text Power; 
+	public PersonVariable Hts;
+	List<PersonVariable> lizt = new List<PersonVariable>();
+	void Start () {
+		Debug.Log("start");
+		Hts = new PersonVariable();
+		Hts.name = "Power";
+		Power.text = Hts.name;
+	}
+	
+	public void Button_Onclick () {
+		Debug.Log("Button Clicked");
+		Hts.value++;
+		Power.text = Hts.name + " " + Hts.value;
+	}
+}
