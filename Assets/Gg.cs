0c56ac61fba83b65e12dbd51a0c26da1029ad2ae
shebkoch <shebkoch@gmail.com>
2018-08-13 15:03:55 +0500

    created button

---
+++ Assets/Gg.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gg : MonoBehaviour {
+
+	public Text Power;
+	
+	private int i = 0;
+	
+	// Use this for initialization
+	void Start () {
+		Debug.Log("start");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Button_Onclick () 
+	{
+		Debug.Log("Button Clicked");
+		
+		i++;
+		Power.text = i.ToString();
+	}
+}
